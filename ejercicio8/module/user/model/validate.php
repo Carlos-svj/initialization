bf1209c478743d79258c0f038fb61198cf036dba
Carlos Orquin <carlosorquinsilvestre@gmail.com>
2021-04-20 21:34:57 +0200

    Ns espere que estiga be avorem, tinc una ganes tremendes

--- ejercicio8/module/user/model/validate.php
+++ ejercicio8/module/user/model/validate.php
@@ -2,17 +2,17 @@
 <?php
     function validate_usuario($texto){
         $reg="/^[a-zA-Z]*$/";
-        return preg_match($reg,$teto);
+        return preg_match($reg,$texto);
     }
     
     function validate_password($texto){
         $reg = "/(?=^.{8,}$)((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/";
-        return preg_match($reg,$teto);
+        return preg_match($reg,$texto);
     }
     
     function validate_nombre($texto){
         $reg="/^[a-zA-Z]*$/";
-        return preg_match($reg,$teto);
+        return preg_match($reg,$texto);
     }
     
     function validate_DNI($dni){
@@ -74,6 +74,7 @@
     }
     
     function validate(){
+
         $check=true;
         
         $v_usuario=$_POST['usuario'];
@@ -87,16 +88,16 @@
         $v_observaciones=$_POST['observaciones'];
         $v_aficion=$_POST['aficion[]'];
         
-        $r_usuario=validate_usuario(v_usuario);
-        $r_password=validate_password(v_password);
-        $r_nombre=validate_nombre(v_nombre);
-        $r_DNI=validate_DNI(v_DNI);
-        $r_sexo=validate_sexo(v_sexo);
-        $r_fecha_nacimiento=validate_fecha(v_fecha_nacimiento);
-        $r_edad=validate_edad(v_edad);
-        $r_idioma=validate_idioma(v_idioma);
-        $r_observaciones=validate_observaciones(v_observaciones);
-        $r_aficion=validate_aficion(v_aficion);
+        $r_usuario=validate_usuario('v_usuario');
+        $r_password=validate_password('v_password');
+        $r_nombre=validate_nombre('v_nombre');
+        $r_DNI=validate_DNI('v_DNI');
+        $r_sexo=validate_sexo('v_sexo');
+        $r_fecha_nacimiento=validate_fecha('v_fecha_nacimiento');
+        $r_edad=validate_edad('v_edad');
+        $r_idioma=validate_idioma('v_idioma');
+        $r_observaciones=validate_observaciones('v_observaciones');
+        $r_aficion=validate_aficion('v_aficion');
         
         if($r_usuario !== 1){
             $error_usuario = " * El usuario introducido no es valido";
--- newproject1/module/vehicle/view/update_vehicle.php
+++ newproject1/module/vehicle/view/update_vehicle.php
@@ -4,106 +4,114 @@
         <table border='0'>
             <tr>
                 <td>id_vehicle: </td>
-                <td><input type="text" id="id_vehicle" name="id_vehicle" placeholder="id_vehicle" value="<?php echo $vehicle['id_vehicle'];?>" readonly/></td>
-                <td><font color="red">
-                    <span id="error_id_vehicle" class="error">
-                        <?php
-                            // echo "$error_id_vehicle";
-                        ?>
-                    </span>
+                <td><input type="text" id="id_vehicle" name="id_vehicle" placeholder="id_vehicle" value="<?php echo $vehicle['id_vehicle']; ?>" readonly /></td>
+                <td>
+                    <font color="red">
+                        <span id="error_id_vehicle" class="error">
+                            <?php
+                            //  echo "$error_id_vehicle";
+                            ?>
+                        </span>
                 </td>
             </tr>
-        
+
             <tr>
                 <td>marca: </td>
-                <td><input type="text" id="marca" name="marca" placeholder="marca" value="<?php echo $vehicle['marca'];?>"/></td>
-                <td><font color="red">
-                    <span id="error_marca" class="error">
-                        <?php
+                <td><input type="text" id="marca" name="marca" placeholder="marca" value="<?php echo $vehicle['marca']; ?>" /></td>
+                <td>
+                    <font color="red">
+                        <span id="error_marca" class="error">
+                            <?php
                             // echo "$error_marca";
-                        ?>
-                    </span>
+                            ?>
+                        </span>
                 </td>
             </tr>
-            
+
             <tr>
                 <td>modelo: </td>
-                <td><input type="text" id="modelo" name="modelo" placeholder="modelo" value="<?php echo $vehicle['modelo'];?>"/></td>
-                <td><font color="red">
-                    <span id="error_modelo" class="error">
-                        <?php
+                <td><input type="text" id="modelo" name="modelo" placeholder="modelo" value="<?php echo $vehicle['modelo']; ?>" /></td>
+                <td>
+                    <font color="red">
+                        <span id="error_modelo" class="error">
+                            <?php
                             // echo "$error_modelo";
-                        ?>
-                    </span>
+                            ?>
+                        </span>
                 </td>
             </tr>
-            
+
             <tr>
                 <td>HP: </td>
-                <td><input type="text" id= "HP" name="HP" placeholder="HP" value="<?php echo $vehicle['HP'];?>"/></td>
-                <td><font color="red">
-                    <span id="error_HP" class="error">
-                        <?php
+                <td><input type="text" id="HP" name="HP" placeholder="HP" value="<?php echo $vehicle['HP']; ?>" /></td>
+                <td>
+                    <font color="red">
+                        <span id="error_HP" class="error">
+                            <?php
                             // echo "$error_HP";
-                        ?>
-                    </span>
+                            ?>
+                        </span>
                 </td>
             </tr>
-            
+
 
             <tr>
                 <td>Km: </td>
-                <td><input type="text" id= "Km" name="Km" placeholder="Km" value="<?php echo $vehicle['Km'];?>"/></td>
-                <td><font color="red">
-                    <span id="error_Km" class="error">
-                        <?php
+                <td><input type="text" id="Km" name="Km" placeholder="Km" value="<?php echo $vehicle['Km']; ?>" /></td>
+                <td>
+                    <font color="red">
+                        <span id="error_Km" class="error">
+                            <?php
                             // echo "$error_Km";
-                        ?>
-                    </span>
+                            ?>
+                        </span>
                 </td>
             </tr>
 
             <tr>
                 <td>Anyo_produccion: </td>
-                <td><input type="text" id="Anyo_produccion" name="Anyo_produccion" placeholder="Anyo_produccion" value="<?php echo $vehicle['Anyo_produccion'];?>"/></td>
-                <td><font color="red">
-                    <span id="error_Anyo_produccion" class="error">
-                        <?php
+                <td><input type="text" id="Anyo_produccion" name="Anyo_produccion" placeholder="Anyo_produccion" value="<?php echo $vehicle['Anyo_produccion']; ?>" /></td>
+                <td>
+                    <font color="red">
+                        <span id="error_Anyo_produccion" class="error">
+                            <?php
                             // echo "$error_Anyo_produccion";
-                        ?>
-                    </span>
+                            ?>
+                        </span>
                 </td>
-                
+
             </tr>
 
-           
+
 
             <tr>
                 <td>color: </td>
-                <td><input type="text" id= "color" name="color" placeholder="color" value="<?php echo $vehicle['color'];?>"/></td>
-                <td><font color="red">
-                    <span id="error_color" class="error">
-                        <?php
+                <td><input type="text" id="color" name="color" placeholder="color" value="<?php echo $vehicle['color']; ?>" /></td>
+                <td>
+                    <font color="red">
+                        <span id="error_color" class="error">
+                            <?php
                             // echo "$error_color";
-                        ?>
-                    </span>
+                            ?>
+                        </span>
                 </td>
             </tr>
 
             <tr>
                 <td>precio: </td>
-                <td><input type="text" id= "precio" name="precio" placeholder="precio" value="<?php echo $vehicle['precio'];?>"/></td>
-                <td><font color="red">
-                    <span id="error_color" class="error">
-                        <?php
+                <td><input type="text" id="precio" name="precio" placeholder="precio" value="<?php echo $vehicle['precio']; ?>" /></td>
+                <td>
+                    <font color="red">
+                        <span id="error_color" class="error">
+                            <?php
                             // echo "$error_precio";
-                        ?>
-                    </span>
+                            ?>
+                        </span>
                 </td>
             </tr>
 
             <tr>
-                <td><input type="submit" name="update" id="update"/></td>
+                <td><input type="submit" name="update" id="update" /></td>
                 <td align="right"><a href="index.php?page=controller_vehicle&op=list">Volver</a></td>
             </tr>
         </table>
